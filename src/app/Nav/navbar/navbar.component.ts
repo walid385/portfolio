bcbdea69e9c8d12f2be5884d772ff72bf1245a51
walidboulhazayez <wboulhazayez@gmail.com>
2024-11-16 14:42:17 +0100

    Navbar

--- src/app/Nav/navbar/navbar.component.ts
+++ src/app/Nav/navbar/navbar.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ThemeSwitcherComponent } from '../../Utils/theme-switcher/theme-switcher.component';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, ThemeSwitcherComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
-  constructor() {}
-
+export class NavbarComponent 
+{
 }
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: '1rem',
+    },
   },
   plugins: [
     require('daisyui'),

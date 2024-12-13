c5ac23460247681cf0437e92f9bef9161d58cab8
Walid Boulhazayez <walid.boulhazayez@youtility.ch>
2024-12-13 17:06:46 +0100

    Projects

--- src/app/Nav/navbar/navbar.component.ts
+++ src/app/Nav/navbar/navbar.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { ThemeSwitcherComponent } from '../../Utils/theme-switcher/theme-switcher.component';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FontAwesomeModule, ThemeSwitcherComponent],
+  imports: [FontAwesomeModule, ThemeSwitcherComponent, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
--- src/app/Pages/wbpulse/wbpulse.component.ts
+++ src/app/Pages/wbpulse/wbpulse.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-wbpulse',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './wbpulse.component.html',
   styleUrl: './wbpulse.component.scss'
 })
 export class WbpulseComponent {
-
+  activeProject: string = 'project1';
 }

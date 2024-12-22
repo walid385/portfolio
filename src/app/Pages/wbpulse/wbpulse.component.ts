21a710a5bcb4fe3feaccab6e57b8b56b7cf11c82
walidboulhazayez <wboulhazayez@gmail.com>
2024-12-22 22:07:56 +0100

    Corrections

--- src/app/Pages/wbpulse/wbpulse.component.ts
+++ src/app/Pages/wbpulse/wbpulse.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-wbpulse',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './wbpulse.component.html',
   styleUrl: './wbpulse.component.scss'
 })
